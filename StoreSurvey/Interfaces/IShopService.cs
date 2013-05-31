95abc6321f7b7b4e1c9631ddabeb38708ae018fb
hasanIqbalAnik <hasan.iqbal.anik@gmail.com>
2013-05-31 15:22:34 +0600

    minor

---
+++ StoreSurvey/Interfaces/IShopService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreSurvey.Interfaces
+{
+    public interface IShopService
+    {
+        List<Shop> GetAllShops();
+        Shop GetShopById(int shopId);
+        List<Shop> TakeNShops(int n);
+        List<Shop> TakeNShopsAfterX(int n, int x);
+    }
+}
